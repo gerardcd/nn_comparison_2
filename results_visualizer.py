1018100f9a3941aac2963e82412ff27182d05c1d
Gerard <gerard.cegarraduenas@gmail.com>
2019-04-25 23:18:42 +0200

    accuracy plots

--- results_visualizer.py
+++ results_visualizer.py
@@ -1,7 +1,4 @@
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from mpl_toolkits.mplot3d import Axes3D
 import pandas
 
 plt.close('all')
@@ -21,19 +18,28 @@ TEST_ACC = -2
 TIME = -1
 
 def plot_grouped(df, group_by, x, y, title):
-
     fig, ax = plt.subplots(figsize=(8, 6))
     for label, df in df.groupby(group_by):
         df.plot(x=x, y=y, ax=ax, label=group_by + ' ' + str(label), title=title)
+
     plt.legend()
+    plt.show()
 
 nn_results = pandas.read_csv('results/nn_results.csv')
 rnn_results = pandas.read_csv('results/rnn_results.csv')
 lstm_results = pandas.read_csv('results/lstm_results.csv')
 
-fig, ax = plt.subplots(figsize=(8,6))
 
-plot_grouped(lstm_results, group_by='embedding_size', x='hidden_size', y='test_accuracy', title='a')
-plot_grouped(lstm_results, group_by='hidden_size', x='embedding_size', y='test_accuracy', title='a')
+nn_results.plot(x='hidden_size', y='test_accuracy', title='NN test accuracy for hidden size')
 plt.show()
+
+plot_grouped(rnn_results, group_by='embedding_size', x='hidden_size', y='test_accuracy',
+             title='RNN test accuracy for hidden size')
+plot_grouped(rnn_results, group_by='hidden_size', x='embedding_size', y='test_accuracy',
+             title='RNN test accuracy for embedding size')
+
+plot_grouped(lstm_results, group_by='embedding_size', x='hidden_size', y='test_accuracy',
+             title='LSTM test accuracy for hidden size')
+plot_grouped(lstm_results, group_by='hidden_size', x='embedding_size', y='test_accuracy',
+             title='LSTM test accuracy for embedding size')
 
